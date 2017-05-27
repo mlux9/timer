bad52938fe10dd8018e1e9192c462d8d1f791cc2
Michelle Lu <michelle.lu9@gmail.com>
2017-05-28 01:16:45 +1000

    Cleaned up code and comments

--- timer.js
+++ timer.js
@@ -1,39 +1,42 @@
 var canvas = document.getElementById("timerCanvas");
 canvas.width = 300;
 canvas.height = 300;
-
 var ctx = canvas.getContext("2d");
 
+var Timer = new TimerObj();
+drawTimer();
+
+/*
+https://code.tutsplus.com/tutorials/how-to-draw-a-pie-chart-and-doughnut-chart-using-javascript-and-html5-canvas--cms-27197
+*/
 function drawTimer() {
 	ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-	// Draw timer circle outline
 	centreX = canvas.width / 2;
 	centreY = canvas.height / 2;
 	radius = 100;
 	startAngle = 0;
 	endAngle = 2*Math.PI;
 
+	// Draw clock outline
 	ctx.beginPath();
 	ctx.arc(centreX, centreY, radius, startAngle, endAngle);
 	ctx.stroke();
 
-	// Draw remaining time slice
+	drawMarkings();
 
-	// Minutes
+	// Draw minutes slice 
 	startAngle = 1.5*Math.PI;
 	endAngle = (((2*Math.PI / 60) / 60) * ((Timer.minutes * 60) + (Timer.seconds))) - 0.5*Math.PI;
 	endAngle = clampAngle(endAngle);
-	//if (Timer.minutes !== 0) {
-		ctx.fillStyle = Timer.minuteColour;
-		ctx.beginPath();
-		ctx.moveTo(centreX, centreY);
-		ctx.arc(centreX, centreY, radius, startAngle, endAngle);
-		ctx.closePath();
-		ctx.fill();
-	//}
+	ctx.fillStyle = Timer.minuteColour;
+	ctx.beginPath();
+	ctx.moveTo(centreX, centreY);
+	ctx.arc(centreX, centreY, radius, startAngle, endAngle);
+	ctx.closePath();
+	ctx.fill();
 
-	// Seconds 
+	// Draw seconds hand 
 	secondsAngle = (2*Math.PI / 60 * Timer.seconds) - 0.5*Math.PI;
 	secondsAngle = clampAngle(secondsAngle);
 	endX = centreX + (radius * Math.cos(secondsAngle));
@@ -43,15 +46,13 @@ function drawTimer() {
 	ctx.lineTo(endX, endY);
 	ctx.strokeStyle = Timer.secondColour;
 	ctx.stroke();
-
-	drawTimerFace();
 }
 
-// Draws the minute markings on the timer face 
-function drawTimerFace() {
+// Draw the minute markings on the clock face 
+function drawMarkings() {
 	for (var i = 0; i < 60; ++i) {
 		var angle = (2*Math.PI / 60 * i);
-		if (i%5 === 0) { 
+		if (i%5 === 0) { // Draw the 5 minute interval markings longer 
 			var start_from_x = centreX + ((radius - 15) * Math.cos(angle));
 			var start_from_y = centreY + ((radius - 15) * Math.sin(angle));
 		} else {
@@ -68,7 +69,7 @@ function drawTimerFace() {
 	}
 }
 
-// Clamps angle between 2pi > ANGLE >= 0
+// 2*PI > angle >= 0
 function clampAngle(angle) {
 	if (angle >= 2*Math.PI) {
 		return angle - 2*Math.PI;
@@ -79,14 +80,12 @@ function clampAngle(angle) {
 	return angle;
 }
 
-var Timer = new TimerObj();
-
 function getTime() {
 	var description = "";
 	var minutes = 0;
 	var seconds = 0;
 
-	// Absolute values 
+	// TODO: Check valid input
 	if (document.getElementById('task_description').value) {
 		description = document.getElementById('task_description').value;
 	}
@@ -97,17 +96,35 @@ function getTime() {
 		seconds = document.getElementById('seconds').value;
 	}
 
-	var str = "Timer set to " + minutes + " minutes and " + seconds + " seconds. <br> Task description is " + description + "<br>";
-
+	var str = "Timer set to " + minutes + " minutes and " + seconds + " seconds. <br> Task description: " + description + "<br>";
 	document.getElementById("time_set").innerHTML = str;
 
 	var time = new Task_Time(minutes, seconds);
 	var remaining_time = new Task_Time(minutes, seconds);
 	var task = new Task(description, time, remaining_time);
-
-	document.getElementById("time_left").innerHTML = "Time left: " + minutes + "minutes and " + seconds + " seconds."
+	document.getElementById("time_left").innerHTML = "Time left: " + minutes + " minutes and " + seconds + " seconds."
 	setTimer(task);
-	
+}
+
+function startTimer() {
+	if (Timer != null) {
+		Timer.timer = setInterval(function() {
+			--Timer.seconds;
+			var time_left = (Timer.minutes * 60) + Timer.seconds; // Time left in seconds
+			
+			if (time_left <= 0) {
+				clearInterval(Timer.timer);
+				document.getElementById("time_left").innerHTML = "Timed out";
+			} else {
+				if (Timer.seconds < 0) {
+					Timer.seconds = 59;
+					Timer.minutes = Timer.minutes - 1;
+				}
+				document.getElementById("time_left").innerHTML = "Time left: " + Timer.minutes + " minutes and " + Timer.seconds + " seconds."
+			}
+			drawTimer();
+		}, 1000);
+	}
 }
 
 function setTimer(Task) {
@@ -116,37 +133,13 @@ function setTimer(Task) {
 	if (Timer.timer) { clearInterval(Timer.timer); }
 	Timer.minutes = Task.time.minutes;
 	Timer.seconds = Task.time.seconds;
-
 	Timer.running = true;
-
 	drawTimer();
-
-	Timer.timer = setInterval(function() {
-		var time_left = (Timer.minutes * 60) + Timer.seconds - 1; // Time left in seconds
-		
-		Timer.seconds = Timer.seconds - 1;
-		if (time_left <= 0) {
-			clearInterval(Timer.timer);
-			document.getElementById("time_left").innerHTML = "Timed out";
-		} else {
-			
-			if (Timer.seconds < 0) {
-				Timer.seconds = 59;
-				Timer.minutes = Timer.minutes - 1;
-			}
-			document.getElementById("time_left").innerHTML = "Time left: " + Timer.minutes + "minutes and " + Timer.seconds + " seconds."
-
-			
-		}
-
-		drawTimer();
-	
-	}, 1000);
+	startTimer();
 
 	document.getElementById("stop_button").style.visibility = "visible";
 	document.getElementById("pause_button").style.visibility = "visible";
 	document.getElementById("pause_button").innerHTML = "Pause";
-
 }
 
 function stopTimer() {
@@ -157,57 +150,27 @@ function stopTimer() {
 	document.getElementById("pause_button").style.visibility = "hidden";
 }
 
+// Pauses or unpauses the timer 
 function pauseTimer() {
-	// Check whether to pause or resume
 	if (Timer.running === true) { // Pause
 		if (Timer.timer) { clearInterval(Timer.timer); }
 		Timer.running = false;
 		Timer.task.remaining_time = new Task_Time(Timer.minutes, Timer.seconds);
+
 		document.getElementById("pause_button").innerHTML = "Resume";
 	} else { // Resume
-		Timer.running = true;
-		
-		// Below is repeating setTimer...just changed the values of Timer.minutes and Timer.seconds
 		if (Timer.timer) { clearInterval(Timer.timer); }
-		printTask(Timer.task);
 		Timer.minutes = Timer.task.remaining_time.minutes;
 		Timer.seconds = Timer.task.remaining_time.seconds;
-
+		Timer.running = true;
 		drawTimer();
-
-		Timer.timer = setInterval(function() {
-			var time_left = (Timer.minutes * 60) + Timer.seconds - 1; // Time left in seconds
-
-			if (time_left <= 0) {
-				clearInterval(Timer.timer);
-
-				document.getElementById("time_left").innerHTML = "Timed out";
-			} else {
-				Timer.seconds = Timer.seconds - 1;
-				if (Timer.seconds < 0) {
-					Timer.seconds = 59;
-					Timer.minutes = Timer.minutes - 1;
-				}
-				document.getElementById("time_left").innerHTML = "Time left: " + Timer.minutes + "minutes and " + Timer.seconds + " seconds.";
-			}
-
-			drawTimer();			
-
-		}, 1000);
+		startTimer();
 
 		document.getElementById("pause_button").innerHTML = "Pause";
 	}
-	
-
 }
 
-
-// function Timer = {
-// 	task: 
-// 	minutes: 
-// 	seconds: 
-// }
-
+// TODO: Rename to Timer? 
 function TimerObj() {
 	this.timer = null;
 	this.task = null;
@@ -219,11 +182,9 @@ function TimerObj() {
 	this.secondColour = "#000000";
 }
 
-function Task(description, task_time, remaining_time) { 
-	//this.id = //randomise id number
+function Task(description, task_time_obj, remaining_time) { 
 	this.description = description;
-	// list:
-	this.time = task_time; // Task_Time 
+	this.time = task_time_obj;
 	this.remaining_time = remaining_time;
 }
 
@@ -233,11 +194,18 @@ function Task_Time(minutes, seconds) {
 	this.seconds = Number(seconds);
 }
 
+function changeColour() {
+	Timer.minuteColour = document.getElementById("minuteColour").value;
+	Timer.secondColour = document.getElementById("secondColour").value;
+}
+
+// Debugging 
 function printTaskTime(Task_Time) {
 	console.log("Minutes: " + Task_Time.minutes);
 	console.log("Seconds: " + Task_Time.seconds);
 }
 
+// Debugging 
 function printTask(Task) {
 	console.log("Description: " + Task.description);
 	console.log("Time: ");
@@ -245,14 +213,3 @@ function printTask(Task) {
 	console.log("Remaining Time: " );
 	printTaskTime(Task.remaining_time);
 }
-
-function changeColour() {
-	Timer.minuteColour = document.getElementById("minuteColour").value;
-	Timer.secondColour = document.getElementById("secondColour").value;
-}
-
-
-/*
-https://code.tutsplus.com/tutorials/how-to-draw-a-pie-chart-and-doughnut-chart-using-javascript-and-html5-canvas--cms-27197
-*/
-
